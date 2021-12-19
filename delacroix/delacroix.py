b2c432976717dd26016977f70f9ab44fa9f2aa36
BuildTools <unconfigured@null.spigotmc.org>
2021-12-19 03:30:04 -0300

    bug

--- delacroix/delacroix.py
+++ delacroix/delacroix.py
@@ -184,7 +184,7 @@ class Delacroix(commands.Cog):
         #    await self.bot.di.update_guild_market(ctx.guild, market)
         await self.config.guild(ctx.guild).market.set(market)
 
-        await ctx.send((await _(ctx, "Item listed with ID {}")).format(id))
+        await ctx.send((await _(self, ctx, "Item listed with ID {}")).format(id))
 
     @commands.group(aliases=["m", "auction"], invoke_without_command=True)
     async def market(self, ctx):
@@ -238,11 +238,11 @@ class Delacroix(commands.Cog):
 
         fin = [[x['id'], f"{x['cost']} {currency}", x['item'], str(y), f"x{x['picture']}"] for x, y in
                zip(chunks[i], users)]
-        fin.insert(0, [await _(ctx, "ID"),
-                       await _(ctx, "COST"),
-                       await _(ctx, "ITEM"),
-                       await _(ctx, "OWNER"),
-                       await _(ctx, "PICTURE")])
+        fin.insert(0, [await _(self, ctx, "ID"),
+                       await _(self, ctx, "COST"),
+                       await _(self, ctx, "ITEM"),
+                       await _(self, ctx, "OWNER"),
+                       await _(self, ctx, "PICTURE")])
         embed.description = "```\n{}\n```".format(self.bot.format_table(fin))
 
         max = len(chunks) - 1
@@ -277,11 +277,11 @@ class Delacroix(commands.Cog):
                     users = get(ctx.guild.members, id=[x["user"] for x in chunks[i]])
                     fin = [[x['id'], f"{x['cost']} dollars", x['item'], str(y), f"x{x['picture']}"] for x, y in
                            zip(chunks[i], users)]
-                    fin.insert(0, [await _(ctx, "ID"),
-                                   await _(ctx, "COST"),
-                                   await _(ctx, "ITEM"),
-                                   await _(ctx, "OWNER"),
-                                   await _(ctx, "PICTURE")])
+                    fin.insert(0, [await _(self, ctx, "ID"),
+                                   await _(self, ctx, "COST"),
+                                   await _(self, ctx, "ITEM"),
+                                   await _(self, ctx, "OWNER"),
+                                   await _(self, ctx, "PICTURE")])
                     embed.description = "```\n{}\n```".format(self.bot.format_table(fin))
 
                     await msg.edit(embed=embed)
@@ -295,11 +295,11 @@ class Delacroix(commands.Cog):
                     users = get(ctx.guild.members, id=[x["user"] for x in chunks[i]])
                     fin = [[x['id'], f"{x['cost']} dollars", x['item'], str(y), f"x{x['picture']}"] for x, y in
                            zip(chunks[i], users)]
-                    fin.insert(0, [await _(ctx, "ID"),
-                                   await _(ctx, "COST"),
-                                   await _(ctx, "ITEM"),
-                                   await _(ctx, "OWNER"),
-                                   await _(ctx, "PICTURE")])
+                    fin.insert(0, [await _(self, ctx, "ID"),
+                                   await _(self, ctx, "COST"),
+                                   await _(self, ctx, "ITEM"),
+                                   await _(self, ctx, "OWNER"),
+                                   await _(self, ctx, "PICTURE")])
                     embed.description = "```\n{}\n```".format(self.bot.format_table(fin))
 
                     await msg.edit(embed=embed)
